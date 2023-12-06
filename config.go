80cfb2c405cb737cd7f78527050ec6edb5be8138
Fish-watching-the-sea <99875158+Fish-watching-the-sea@users.noreply.github.com>
2023-12-06 13:44:15 +0800

    把新建config结构体的方法变成公共方法

--- config.go
+++ config.go
@@ -22,6 +22,41 @@ func NewConfig(trackerAddr []string, maxConns int) *config {
 	}
 }
 
+func NewConfigWithConfFile(configName string) (*config, error) {
+	config := &config{}
+	f, err := os.Open(configName)
+	if err != nil {
+		return nil, err
+	}
+	splitFlag := "\n"
+	if runtime.GOOS == "windows" {
+		splitFlag = "\r\n"
+	}
+	reader := bufio.NewReader(f)
+	for {
+		line, err := reader.ReadString('\n')
+		line = strings.TrimSuffix(line, splitFlag)
+		str := strings.SplitN(line, "=", 2)
+		cmd := strings.Trim(str[0], " ")
+		switch cmd {
+		case "tracker_server":
+			config.trackerAddr = append(config.trackerAddr, strings.Trim(str[1], " "))
+		case "maxConns":
+			config.maxConns, err = strconv.Atoi(strings.Trim(str[1], " "))
+			if err != nil {
+				return nil, err
+			}
+		}
+		if err != nil {
+			if err == io.EOF {
+				return config, nil
+			}
+			return nil, err
+		}
+	}
+	return config, nil
+}
+
 func newConfig(configName string) (*config, error) {
 	config := &config{}
 	f, err := os.Open(configName)
